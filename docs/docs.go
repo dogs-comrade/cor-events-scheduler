9967000228837eb8bd89a44900168475810648fb
MrDucheved <ducheved@mail.ru>
2024-11-04 17:33:16 +0300

    refactor: update API host in documentation files to scheduler.xilonen.ru

--- docs/docs.go
+++ docs/docs.go
@@ -1164,7 +1164,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8282",
+	Host:             "scheduler.xilonen.ru",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Event Scheduler API",
